600d7a5dabaaa18f208f791d2f953c4a05acbe55
oleg-kuibar <oleg.kuibar@a4media.com>
2022-09-01 11:11:15 +0300

    1 file modified

--- components/widgets/radio-with-icons.widget.js
+++ components/widgets/radio-with-icons.widget.js
@@ -5,15 +5,15 @@ import RadioButton from '../RadioButton';
 
 export const RadioWithIcons = (props) => {
   return (
-    <RadioGroup value={props.value} onChange={props.onChange}>
-      {props.options.enumOptions.map((element, index) => {
-        const icon = React.createElement(props.options.icons[index]);
-        const selectedIcon = React.createElement(
-          props.options.selectedIcons[index]
-        );
+    <HStack spacing="40px">
+      <RadioGroup value={props.value} onChange={props.onChange}>
+        {props.options.enumOptions.map((element, index) => {
+          const icon = React.createElement(props.options.icons[index]);
+          const selectedIcon = React.createElement(
+            props.options.selectedIcons[index]
+          );
 
-        return (
-          <HStack spacing="40px">
+          return (
             <RadioButton
               key={`${index}_${element.value}`}
               title={element.label}
@@ -21,21 +21,9 @@ export const RadioWithIcons = (props) => {
               icon={icon}
               selectedIcon={selectedIcon}
             />
-            {/* <RadioButton
-          title={availableAttributes.items[1].label}
-          value={availableAttributes.items[1].value}
-          icon={<DefaultMale />}
-          selectedIcon={<SelectedMale />}
-        />
-        <RadioButton
-          title={availableAttributes.items[2].label}
-          value={availableAttributes.items[2].value}
-          icon={<DefaultFemale />}
-          selectedIcon={<SelectedFemale />}
-        /> */}
-          </HStack>
-        );
-      })}
-    </RadioGroup>
+          );
+        })}
+      </RadioGroup>
+    </HStack>
   );
 };
